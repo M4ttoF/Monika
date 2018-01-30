508a5c5d002f0492f1c9c51292ba09750ef66cc1
Matt Farias <ezioblade64@gmail.com>
2018-01-29 20:38:07 -0500

    switched over to dictionaries

--- Monika/MainBot.py
+++ Monika/MainBot.py
@@ -4,9 +4,9 @@ import asyncio
 import random
 import os
 import sys
-#from riotwatcher import RiotWatcher
+from riotwatcher import RiotWatcher
 
-#watcher = RiotWatcher('RGAPI-8b67a641-0366-4c60-8a91-73a07d8764f3')
+watcher = RiotWatcher('RGAPI-8b67a641-0366-4c60-8a91-73a07d8764f3')
 
 client = discord.Client()
 
@@ -20,6 +20,130 @@ SAFE = '403296596424654859'		#Testerino
 
 #This dictionary stores all the mod roles for the servers the bot is in
 modRoles={}
+
+global messageCom
+
+messageCom=None
+
+############################
+#		Owner Commands
+async def killCom(message):
+	if message.author.id==ADMIN:
+		await client.send_message(message.channel, 'Goodbye!')
+		client.logout()
+		client.close()
+		sys.exit()
+
+async def serversCom(message):
+	if message.author.id==ADMIN:
+		msg=''
+		for server in client.servers:
+			msg+= server.name+'\t'+server.id
+			msg+='\n'
+		await client.send_message(message.channel, msg)
+
+async def modrolesCom(self, message):
+	if message.auth.id==ADMIN:
+		print(modRoles)
+
+
+###################################
+#		Mod commands
+
+
+async def purgeCom(message):
+	if message.channel.permissions_for(message.author).manage_messages:
+		line=message.content.split()
+		await purge(message,line)
+	else:
+		await client.send_message(message.channel, 'Invalid Permissions')	
+
+async def setmodroleCom(message):
+	if message.channel.permissions_for(message.author).administrator:
+		roles=message.role_mentions
+		if len(roles)==0:
+			if message.server in modRoles:
+				
+				del(modRoles[message.server])
+				with open('ModRoles.txt','r') as modRolesFile:
+					lines=modRolesFile.read()
+
+				with open('ModRoles.txt','w') as modRolesFile:
+					for line in lines:
+						if not line.startswith(message.server.id) and len(line.split())==2:
+							modRolesFile.write(line+'\n')
+
+				await client.send_message(message.channel, 'Mod role cleared')
+
+			else:
+				await client.send_message(message.channel, 'No role mentioned!')
+		else:
+			with open('ModRoles.txt','a') as modRolesFile:
+					modRolesFile.write(message.server.id+' '+roles[0].id+'\n')
+
+			await client.send_message(message.channel, 'Mod role set to '+roles[0].name)
+			modRoles[message.server]=roles[0]
+	else:
+		await client.send_message(message.channel, 'Invalid Permissions')
+
+
+
+async def modCom(message):
+	if message.server in modRoles:
+
+		if message.channel.permissions_for(auth).administrator:
+			users=message.mentions
+			await addRoles(users,[modRoles[message.server]],message.channel)
+
+		else:
+			await client.send_message(message.channel, 'Invalid Permissions')
+	else:
+		await client.send_message(message.channel, 'No mod role set up!\nUse "!setmodrole <Role>" to set the role')
+
+
+async def demodCom(message):
+	if message.channel.permissions_for(message.author).administrator:
+		if message.server in modRoles:
+			users=message.mentions
+			if len(users)==0:
+				await client.send_message(message.channel, 'No users mentioned!')
+			else:
+				await removeRoles(users,[modRoles[message.server]],message.channel)
+		else:
+			await client.send_message(message.channel, 'No mod role set up!\nUse "!modrole <Role>" to set the role')
+
+	else:
+		await client.send_message(message.channel, 'Invalid Permissions')
+
+
+######################################
+#		Regular Commands
+
+async def rollCom(message):
+	line = message.content.split()
+	if len(line)<1:		
+		await client.send_message(message.channel, 'Invalid usage')
+	elif len(line)==1:
+		await roll(message,1,6)
+	elif len(line)==2:
+		await roll(message,1,int(line[1]))
+	else:
+		await roll(message,int(line[1]),int(line[2]))
+
+
+
+
+
+
+
+commands={
+	'!kill':killCom,'!servers': serversCom,'!mod':modCom,
+	'!setmodrole':setmodroleCom,'!demod':demodCom,'!roll':rollCom,
+	'!modroles':modrolesCom
+	}
+
+
+
 
 
 
@@ -48,131 +172,13 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-	global mimic, chanNames
-	try:
-		if mimic==None:
-			print("oi")
-	except NameError:
-		chanNames=[]
-		for s in client.servers:
-			if s.id==SAFE:
-				mimic=s
-				break
-		for c in mimic.channels:
-			if c.type==discord.ChannelType.text:
-				chanNames.append(c.name)
-		print(mimic.id)
-
-    
-	###########################################
-	#Owner commands
-	auth=message.author
-	if auth.id==ADMIN:
-		if message.content.startswith('!servers'):
-			msg=''
-			for server in client.servers:
-				msg+= server.name+'\t'+server.id
-				msg+='\n'
-			await client.send_message(message.channel, msg)
-		elif message.content.startswith('!kill'):
-			await client.send_message(message.channel, 'Goodbye!')
-			client.logout()
-			client.close()
-			sys.exit()
-
-		elif message.content.startswith('!modroles'):
-			print(modRoles)
-
-
-
-	##########################################\
-	#mod commands
-	if message.content.startswith('!purge'):
-		if message.channel.permissions_for(auth).manage_messages:
-			line=message.content.split()
-			await purge(message,line)
-		else:
-			await client.send_message(message.channel, 'Invalid Permissions')	
+	global mimic, chanNames, messageCom
+	messageCom=message
+	
+	if message.content.startswith('!'):
+		if message.content.lower().split()[0] in commands:
+			await commands[message.content.lower().split()[0]](message=message)
 		
-
-
-
-	#Admin only commands
-	elif message.content.startswith('!setmodrole'):
-		if message.channel.permissions_for(auth).administrator:
-			roles=message.role_mentions
-			if len(roles)==0:
-				if message.server in modRoles:
-					
-					del(modRoles[message.server])
-					with open('ModRoles.txt','r') as modRolesFile:
-						lines=modRolesFile.read()
-
-					with open('ModRoles.txt','w') as modRolesFile:
-						for line in lines:
-							if not line.startswith(message.server.id) and len(line.split())==2:
-								modRolesFile.write(line+'\n')
-
-					await client.send_message(message.channel, 'Mod role cleared')
-
-				else:
-					await client.send_message(message.channel, 'No role mentioned!')
-			else:
-				with open('ModRoles.txt','a') as modRolesFile:
-						modRolesFile.write(message.server.id+' '+roles[0].id+'\n')
-
-				await client.send_message(message.channel, 'Mod role set to '+roles[0].name)
-				modRoles[message.server]=roles[0]
-		else:
-			await client.send_message(message.channel, 'Invalid Permissions')
-
-			
-
-	elif message.content.startswith('!mod'):
-
-		if message.server in modRoles:
-
-			if message.channel.permissions_for(auth).administrator:
-				users=message.mentions
-				await addRoles(users,[modRoles[message.server]],message.channel)
-
-			else:
-				await client.send_message(message.channel, 'Invalid Permissions')
-		else:
-			await client.send_message(message.channel, 'No mod role set up!\nUse "!setmodrole <Role>" to set the role')
-
-
-
-	elif message.content.startswith('!demod'):
-		if message.channel.permissions_for(auth).administrator:
-			if message.server in modRoles:
-				users=message.mentions
-				if len(users)==0:
-					await client.send_message(message.channel, 'No users mentioned!')
-				else:
-					await removeRoles(users,[modRoles[message.server]],message.channel)
-			else:
-				await client.send_message(message.channel, 'No mod role set up!\nUse "!moderole <Role>" to set the role')
-
-		else:
-			await client.send_message(message.channel, 'Invalid Permissions')
-	#regular commands
-
-	elif message.content.startswith('!sleep'):
-		await asyncio.sleep(5)
-		await client.send_message(message.channel, 'Done sleeping')
-
-	elif message.content.startswith('!roll'):
-		line = message.content.split()
-		if len(line)<1:
-			await client.send_message(message.channel, 'Invalid usage')
-		elif len(line)==1:
-			await roll(message,1,6)
-		elif len(line)==2:
-			await roll(message,1,int(line[1]))
-		else:
-			await roll(message,int(line[1]),int(line[2]))
-
 	
 
 	###############################################
